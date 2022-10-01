326a32385ab9d03df0d217694eae12c98e144bf4
vuphan0804 <vuphan0804@gmail.com>
2022-10-01 22:23:19 +0700

    First Edit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <div
       id="header"
-      className={`flex z-20 top-0 left-0 right-0 fixed bg-gray-900 justify-between items-center h-24 text-textPrimary mx-auto px-10 transition-primary ${headerFixed}`}
+      className={`flex z-20 top-0 left-0 right-0 fixed bg-gray-900 justify-between items-center h-24 text-textPrimary mx-auto px-28 transition-primary ${headerFixed}`}
     >
       <div className="flex items-center justify-between gap-2 text-2xl font-bold">
         <span>
@@ -26,23 +26,25 @@ const Header = () => {
         </span>
         <h1>VP</h1>
       </div>
-      <ul className="flex justify-between gap-4 text-textSecondary">
-        <li className="hover:text-white">
-          <a href="#intro">Home</a>
-        </li>
-        <li className="hover:text-white">
-          <a href="#myProcess">Process</a>
-        </li>
-        <li className="hover:text-white">
-          <a href="#myProjects">Projects</a>
-        </li>
-        <li className="hover:text-white">
-          <a href="#myExperience">About Me</a>
-        </li>
-        <li className="hover:text-white">
-          <a href="">Blog</a>
-        </li>
-      </ul>
+      <div className="hidden xl:block lg:block">
+        <ul className="flex justify-between gap-4 text-textSecondary">
+          <li className="hover:text-white">
+            <a href="#intro">Home</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#myProcess">Process</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#myProjects">Projects</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="#myExperience">About Me</a>
+          </li>
+          <li className="hover:text-white">
+            <a href="">Blog</a>
+          </li>
+        </ul>
+      </div>
 
       <div className="text-base font-medium">
         <a href="#footer">
