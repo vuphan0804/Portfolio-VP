4f20e497d6f04b4d01091fe9b26528679806ba99
vuphan0804 <vuphan0804@gmail.com>
2022-10-01 22:58:40 +0700

    Complete Responsive 2

--- src/components/Contents/components/hireMe/HireMe.jsx
+++ src/components/Contents/components/hireMe/HireMe.jsx
@@ -8,7 +8,7 @@ const HireMe = () => {
           Have projects in mind? Let’s work{" "}
           <span className="linear-text-secondary">together</span>
         </h1>
-        <p className="px-72 text-textSecondary">
+        <p className="mx-auto text-textSecondary">
           I'm Vu Phan — a front end web developer who works with me looking to
           push creative boundaries.
         </p>
